c68b8765ede6283b17b6945c49420c53ed2c2a4d
cfrdocarmo <73319579+cfrdocarmo@users.noreply.github.com>
2021-04-27 13:57:33 -0300

    Add files via upload

---
+++ bytebank-herdado/src/Designer.java
@@ -0,0 +1,16 @@
+
+public class Designer extends Funcionario{
+
+	@Override
+	public double getBonificacao() {
+		System.out.println("Chama o método de bonificação do Designer");
+		return 200;
+	}
+
+
+	}
+
+	
+	
+	
+
---
+++ bytebank/src/bytebank/TesteReferencia.java
@@ -0,0 +1,29 @@
+package bytebank;
+
+public class TesteReferencia {
+
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 300;
+		
+		System.out.println("Saldo da primeira conta: " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta;
+		
+		System.out.println("Saldo da segunda conta: " + segundaConta.saldo);
+		
+		segundaConta.saldo += 100;
+		System.out.println("Saldo da segunda conta: " + segundaConta.saldo);
+		
+		System.out.println("Saldo da primeira conta: " + primeiraConta.saldo);
+		
+		if (primeiraConta == segundaConta) {
+			System.out.println("Mesma conta!");
+		} else {
+			System.out.println("Contas diferentes!");
+		}
+		
+		System.out.println(primeiraConta);
+		System.out.println(segundaConta);
+	}
+}
